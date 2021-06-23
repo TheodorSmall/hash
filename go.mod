bb4a92a8bfd5d3b8e198e64cf5a950baeab80733
TheodorSmall <Jonathan@florian-reuthner.de>
2021-06-23 19:43:05 +0200

    Implement hash(...) function and write main()

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TheodorSmall/sha512-hash
+
+go 1.16
---
+++ sha512hash.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crypto/sha512"
+	"fmt"
+	"os"
+	"io"
+	"encoding/base64"
+)
+
+func hash(password string) string {
+	hash := sha512.New()
+	io.WriteString(hash, password)
+	decodedhash := hash.Sum(nil)
+	return base64.StdEncoding.EncodeToString(decodedhash)
+}
+
+func main() {
+	if len(os.Args[1:]) < 1 {
+		fmt.Printf("No command line argument to hash")
+		os.Exit(1)
+	}
+
+	password := os.Args[1]
+	fmt.Printf("%s\n", hash(password))
+}
